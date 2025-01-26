c422153acab843e07e6e51a43a8b090893014eed
niceman1234man <yihunietarekegn18@gmail.com>
2025-01-26 10:43:48 -0800

    packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Routes,Route} from 'react-router'
 import Welcome from './components/Welcome'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
 function App() {
   return (
-    <div>App
-    <Welcome/>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={  <Welcome/>}/>
+      <Route path='/login' element={  <Login/>}/>
+      <Route path='/signup' element={  <Signup/>}/>
+    </Routes>
+  
+    </>
   )
 }
 
