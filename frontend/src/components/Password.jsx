6489645f9b4b42003aae44c836de35c94114bd8d
niceman1234man <yihunietarekegn18@gmail.com>
2025-01-27 11:32:57 -0800

    password

---
+++ frontend/src/components/Password.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+
+function Password({ placeholder, value, handleChange }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="relative w-[72%] mx-auto flex justify-center">
+      <input
+        type={showPassword ? 'text' : 'password'}
+        name="password"
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        className="p-3 border w-full m-2"
+        required
+      />
+      <span
+        className="absolute right-4 top-5 cursor-pointer"
+        onClick={() => setShowPassword(!showPassword)}
+        aria-label={showPassword ? "Hide password" : "Show password"}
+      >
+        {showPassword ? <FaRegEyeSlash size={20} /> : <FaRegEye size={20} />}
+      </span>
+    </div>
+  );
+}
+
+export default Password;
