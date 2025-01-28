cb69e518d5ae6a72ee1c10d783638cf257153ef3
niceman1234man <yihunietarekegn18@gmail.com>
2025-01-27 16:18:21 -0800

    password

--- frontend/src/components/Password.jsx
+++ frontend/src/components/Password.jsx
@@ -5,14 +5,14 @@ function Password({ placeholder, value, handleChange }) {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="relative w-[72%] mx-auto flex justify-center">
+    <div className="relative w-[90%] mx-auto flex justify-center mr-0">
       <input
         type={showPassword ? 'text' : 'password'}
         name="password"
         placeholder={placeholder}
         value={value}
         onChange={handleChange}
-        className="px-3 py-2 border w-full m-2 rounded"
+        className="px-3 py-2 border w-full m-2 mr-0"
         required
       />
       <span
