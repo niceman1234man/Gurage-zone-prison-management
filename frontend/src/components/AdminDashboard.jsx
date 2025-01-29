8991949524c504d988e3af0f6aaa158085926d1c
niceman1234man <yihunietarekegn18@gmail.com>
2025-01-29 11:43:06 -0800

    admin dash board

--- frontend/src/components/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 function AdminDashboard() {
   return (
     <div>
-       <h1 className="border-b p-5 text-2xl font-bold">Welcome, Admin!</h1>
-         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-          <div className="border  p-2 font-semibold">
-           <h3>Active Users</h3>
-           <p>23</p>
-          </div>
-          <div className="border p-2 font-semibold shadow">
+      <h1 className="border-b p-5 text-2xl font-bold">Welcome, Admin!</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        <div className="border p-2 font-semibold">
+          <h3>Active Users</h3>
+          <p>23</p>
+        </div>
+        <div className="border p-2 font-semibold shadow">
           <h3>All Users</h3>
           <p>21</p>
-          </div>
-          <div className="border p-2 font-semibold shadow">
-           <h3> Blocked Users</h3>
-           <p>4</p>
-          </div>
-          <div className="border p-2 font-semibold shadow">
+        </div>
+        <div className="border p-2 font-semibold shadow ">
+          <h3>Blocked Users</h3>
+          <p>4</p>
+        </div>
+        <div className="border p-2 font-semibold shadow">
           <h3>Messages</h3>
           <p>5</p>
-          </div>
-          <div className="border p-2 font-semibold shadow">
-            <h3>Setting</h3>
-            <p></p>
-          </div>
-          <img src="" alt="" />
-         </div>   
+        </div>
+        <div className="border p-2 font-semibold shadow">
+          <h3>Settings</h3>
+          <p></p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AdminDashboard
+export default AdminDashboard;
