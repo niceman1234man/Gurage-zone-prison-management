d988e072fa5cb6156afc7de37fa4dd75f2119831
niceman1234man <yihunietarekegn18@gmail.com>
2025-01-30 15:20:01 -0800

    security staff

--- frontend/src/page/SecurityStaff.jsx
+++ frontend/src/page/SecurityStaff.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { RxDashboard } from "react-icons/rx";
 import { HiUserGroup } from "react-icons/hi";
+import { FaAngleDown } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa6";
+import { FiFileText } from "react-icons/fi";
 import { FaUserCheck, FaUserPlus, FaUsersCog, FaUserSlash } from "react-icons/fa";
 import ListofUsers from "../components/Admin/ListofUsers";
 import CreateNewUser from "../components/Admin/CreateNewUser";
@@ -12,6 +15,8 @@ import { useNavigate } from "react-router";
 function SecurityStaff() {
   const navigate = useNavigate();
   const [activeComponent, setActiveComponent] = useState("Dashboard");
+  const [profiletoggle,setProfileToggle]=useState(true);
+  const [inmatetoggle,setInmateToggle]=useState(true);
 
   const renderComponent = () => {
     switch (activeComponent) {
@@ -45,20 +50,38 @@ function SecurityStaff() {
           <li onClick={() => setActiveComponent("Dashboard")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "Dashboard" ? 'bg-green-400' : ''}`}>
             <RxDashboard className="mr-2" size={20} /> Dashboard
           </li>
+          <h2 className="text-xl font-semibold flex items-center">Profile <span className="ml-2">{profiletoggle?<FaAngleDown onClick={()=>setProfileToggle(!profiletoggle)}/>:<FaAngleUp onClick={()=>setProfileToggle(!profiletoggle)}/>}</span></h2>
+          <div className={`${profiletoggle? 'hidden':''} space-y-6`} > 
+          <li onClick={() => setActiveComponent("My Profile")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "My Profile" ? 'bg-green-400' : ''}`}>
+            <RxDashboard className="mr-2" size={20} /> Update Profile
+          </li>
+          <li onClick={() => setActiveComponent("Create New")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "Create New" ? 'bg-green-400' : ''}`}>
+            <FaUserPlus className="mr-2" size={20} /> Manage Issue
+          </li>
+          </div>
+          <h2 className="text-xl font-semibold flex items-center">Manage Inmate <span className="ml-2">{inmatetoggle?<FaAngleDown onClick={()=>setInmateToggle(!inmatetoggle)}/>:<FaAngleUp onClick={()=>setInmateToggle(!inmatetoggle)}/>}</span></h2>
+          <div className={`${inmatetoggle? 'hidden':''} space-y-6`} > 
           <li onClick={() => setActiveComponent("Create New")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "Create New" ? 'bg-green-400' : ''}`}>
             <FaUserPlus className="mr-2" size={20} /> New Inmate
           </li>
           <li onClick={() => setActiveComponent("All Users")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "All Users" ? 'bg-green-400 ' : ''}`}>
-            <HiUserGroup className="mr-2" size={20} /> All Prisoners
+            <HiUserGroup className="mr-2" size={20} /> Inmate List
           </li>
           {/* <li onClick={() => setActiveComponent("Active Users")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "Active Users" ? 'bg-green-400' : ''}`}>
             <FaUserCheck className="mr-2" size={20} /> Active Users
           </li> */}
           <li onClick={() => setActiveComponent("Blocked Users")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "Blocked Users" ? 'bg-green-400' : ''}`}>
-            <FaUserSlash className="mr-2" size={20} /> Reports
+            <FiFileText className="mr-2" size={20} />Generate Report
           </li>
+          <li onClick={() => setActiveComponent("Blocked Users")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "Blocked Users" ? 'bg-green-400' : ''}`}>
+            <FaUserSlash className="mr-2" size={20} />Transfer Prisoner
+          </li>
+          <li onClick={() => setActiveComponent("Blocked Users")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "Blocked Users" ? 'bg-green-400' : ''}`}>
+            <FiFileText className="mr-2" size={20} />Generate Clearance
+          </li>
+          </div>
           <li onClick={() => setActiveComponent("System Information")} className={`hover:bg-gray-300 cursor-pointer flex items-center border-b border-white ${activeComponent === "System Information" ? 'bg-green-400' : ''}`}>
-            <FaUsersCog className="mr-2" size={20} /> System Information
+            <FaUsersCog className="mr-2" size={20} />Setting
           </li>
         </ul>
       </div>
