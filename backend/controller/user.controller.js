3b79bd95fb7e7d7fc2ac5664251e6f07dd4fee56
niceman1234man <yihunietarekegn18@gmail.com>
2025-02-07 14:53:53 -0800

    backend

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,65 @@
+import { User } from "../model/user.model.js";
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config();
+
+export const createAccount=async(req,res)=>{
+const {fullName,email,role,password}=req.body;
+if(!fullName||!email||!password){
+    return res.status(400).json("all fields required");
+}
+const isUser=await User.findOne({email:email});
+if(isUser) return res.status(400).json("User Already existed")
+const newUser=new User({
+    fullName,
+    email,
+    role,
+    password
+});
+await newUser.save();
+const accessToken=jwt.sign({newUser},process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn:"36000m"
+})
+return res.status(201).json({error:false,accessToken,message:"user registered successfully"})
+
+}
+export const login=async(req,res)=>{
+    try {
+        
+   
+    const {email,password}=req.body;
+    if(!email||!password){
+        return res.status(400).json("please all fields")
+    }
+    const userInfo=await User.findOne({email:email})
+    if(!userInfo){
+        return res.status(400).json("user not exist");
+    }
+    if(userInfo.password!==password){
+        return res.status(400).json("password not match");
+    }
+    const user={user:userInfo}
+    const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:"36000m"
+    })
+    res.status(200).json({email,accessToken,meaage:"Login Successfully"});
+} catch (error) {
+  console.log(error)      
+}
+}
+
+export const getUser = async (req, res) => {
+    try {
+        const { user } = req.user; 
+        const userInfo = await User.findOne({ _id: user._id }); // Await the promise
+
+        if (!userInfo) {
+            return res.status(400).json({ message: "User does not exist" });
+        }
+
+        res.status(200).json({user:userInfo});
+    } catch (error) {
+        console.error(error); // Log the error for debugging
+        res.status(500).json({ message: "Server error" });
+    }
+};
---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { connectDb } from './config/db.js';
+import { userRouter } from './router/user.router.js';
+dotenv.config();
+const app =express();
+app.use(express.json());
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials: true,
+    
+// }));
+ app.use('/user/',userRouter);
+ connectDb();
+
+app.listen(5000,()=>{
+    console.log("server running")
+});
+
