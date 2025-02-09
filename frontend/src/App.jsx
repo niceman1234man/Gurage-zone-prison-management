76eb24b491f0675b5c5ad82536a2532eb2f26353
niceman1234man <yihunietarekegn18@gmail.com>
2025-02-09 11:12:10 -0800

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,58 @@
-import React from 'react'
-import {Routes,Route} from 'react-router'
-import Welcome from './components/Welcome'
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
-import Admin from './page/Admin'
-import Visitor from './page/Visitor'
-import VisitorRegister from './page/VisitorRegister'
-import ForegotPassword from './page/ForegotPassword'
-import VisitorDashboard from './page/VisitorDashboard'
-import PoliceOfficer from './page/PoliceOfficer'
-import Inspector from './page/Inspector'
-import SecurityStaff from './page/SecurityStaff'
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import { Routes, Route } from "react-router";
+import Welcome from "./components/Welcome";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Admin from "./page/Admin";
+import Visitor from "./page/Visitor";
+import VisitorRegister from "./page/VisitorRegister";
+import ForegotPassword from "./page/ForegotPassword";
+import VisitorDashboard from "./page/VisitorDashboard";
+import PoliceOfficer from "./page/PoliceOfficer";
+import Inspector from "./page/Inspector";
+import SecurityStaff from "./page/SecurityStaff";
+import { ToastContainer } from "react-toastify";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={  <Welcome/>}/>
-      <Route path='/login' element={  <Login/>}/>
-      <Route path='/signup' element={  <Signup/>}/>
-      <Route path='/admin' element={  <Admin/>}/>
-      <Route path='/visitor' element={  <Visitor/>}/>
-      <Route path='/register' element={  <VisitorRegister/>}/>
-      <Route path='/foregot-password' element={  <ForegotPassword/>}/>
-      <Route path='/visitor-dash' element={  <VisitorDashboard/>}/>
-      <Route path='/police-officer' element={  <PoliceOfficer/>}/>
-      <Route path='/inspector' element={  <Inspector/>}/>
-      <Route path='/security' element={  <SecurityStaff/>}/>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/foregot-password" element={<ForegotPassword />} />
 
-    </Routes>
-  <ToastContainer/>
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+          <Route path="/admin" element={<Admin />} />
+        </Route>
+
+        <Route element={<ProtectedRoute allowedRoles={["visitor"]} />}>
+          <Route path="/visitor-dash" element={<VisitorDashboard />} />
+        </Route>
+
+        <Route element={<ProtectedRoute allowedRoles={["visitor"]} />}>
+          <Route path="/visitor" element={<Visitor />} />
+          <Route path="/register" element={<VisitorRegister />} />
+        </Route>
+
+        <Route element={<ProtectedRoute allowedRoles={["police-officer"]} />}>
+          <Route path="/police-officer" element={<PoliceOfficer />} />
+        </Route>
+
+        <Route element={<ProtectedRoute allowedRoles={["inspector"]} />}>
+          <Route path="/inspector" element={<Inspector />} />
+        </Route>
+
+        <Route element={<ProtectedRoute allowedRoles={["security"]} />}>
+          <Route path="/security" element={<SecurityStaff />} />
+        </Route>
+      </Routes>
+
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
